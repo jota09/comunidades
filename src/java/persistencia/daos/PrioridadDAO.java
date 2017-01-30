221a3d8f79e8db07837edeabb90b5f113ad6586c
jota09 <jesus.ramos@hmt-colombia.com>
2017-01-30 12:55:45 -0500

    [+] Cargar datos en el servidor Clasificados Añadir

---
+++ src/java/persistencia/daos/PrioridadDAO.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia.daos;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import persistencia.conexion.ConexionBD;
+import persistencia.entidades.Prioridad;
+
+/**
+ *
+ * @author ferney.medina
+ */
+public class PrioridadDAO implements GestionDAO{
+
+    @Override
+    public Object getObject(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List getListObject(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public List getListObject() {
+        ArrayList<Prioridad> listPrioridad=new ArrayList<Prioridad>();                
+        Connection con=null;
+        try
+        {
+            con=ConexionBD.obtenerConexion();
+            String query="SELECT * FROM prioridad WHERE activo=1";
+            PreparedStatement pS=con.prepareStatement(query);
+            ResultSet rS=pS.executeQuery();            
+            while(rS.next()){
+                Prioridad prio=new Prioridad(rS.getInt("codigo"), rS.getString("nombre"), rS.getInt("valor"), rS.getShort("activo"));
+                prio.setCodigo(rS.getInt("codigo"));
+                prio.setNombre(rS.getString("nombre"));
+                listPrioridad.add(prio);
+            }
+            rS.close();
+            pS.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally{
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ArticuloDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return listPrioridad;
+    }   
+    
+    @Override
+    public int updateObject(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int insertObject(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        @Override
+    public int getCount(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteObject(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
