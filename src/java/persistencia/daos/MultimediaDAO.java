27850d139deefbcd29fc596901f9aa9c5ca592bf
ferney.medina <ferney.medina@HMT-111.hmt.local>
2017-01-31 08:42:16 -0500

    añadiendo multimedia

--- src/java/persistencia/daos/MultimediaDAO.java
+++ src/java/persistencia/daos/MultimediaDAO.java
@@ -5,22 +5,13 @@
  */
 package persistencia.daos;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import persistencia.conexion.ConexionBD;
-import persistencia.entidades.TipoMultimedia;
 
 /**
  *
  * @author ferney.medina
  */
-public class TipoMultimediaDAO implements GestionDAO{
+public class MultimediaDAO implements GestionDAO{
 
     @Override
     public int getCount(Object object) {
@@ -34,28 +25,7 @@ public class TipoMultimediaDAO implements GestionDAO{
 
     @Override
     public List getListObject(Object object) {
-        String nombreTpMult = (String) object;
-        ArrayList<TipoMultimedia> listTpMult = new ArrayList<TipoMultimedia>();
-        try {
-            Connection con=null;
-            con=ConexionBD.obtenerConexion();
-            String query = "SELECT * FROM tipo_multimedia WHERE nombre=?";
-            PreparedStatement pS=con.prepareStatement(query);
-            pS.setString(1, nombreTpMult);
-            ResultSet rS=pS.executeQuery();
-            while(rS.next())
-            {
-                TipoMultimedia tpMult=new TipoMultimedia();
-                tpMult.setCodigo(rS.getInt("codigo"));
-                tpMult.setExtension(rS.getString("extension"));
-                listTpMult.add(tpMult);
-            }
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(ArticuloDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(ArticuloDAO.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return listTpMult;        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
