2ed4e4045a43c258f8fbde76c429c07281f4594e
jota09 <jesus.ramos@hmt-colombia.com>
2017-01-31 17:50:43 -0500

    [+] Añadir Prioridad DAO

--- src/java/controladores/ClasificadoControlador.java
+++ src/java/controladores/ClasificadoControlador.java
@@ -7,9 +7,15 @@ package controladores;
 
 import fachada.ArticuloFachada;
 import fachada.CategoriaFachada;
+import fachada.PrioridadFachada;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +26,7 @@ import org.json.simple.JSONObject;
 import persistencia.entidades.Articulo;
 import persistencia.entidades.ArticuloEstado;
 import persistencia.entidades.Categoria;
+import persistencia.entidades.Prioridad;
 import persistencia.entidades.TipoArticulo;
 import persistencia.entidades.Usuario;
 
@@ -42,79 +49,87 @@ public class ClasificadoControlador extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-System.out.println(request);
-        if(request.getParameter("opc")!=null)
-        {
-            int opcion = Integer.parseInt(request.getParameter("opc"));       
+        System.out.println(request);
+        if (request.getParameter("opc") != null) {
+            int opcion = Integer.parseInt(request.getParameter("opc"));
             switch (opcion) {
                 case 1:
                     recuperarCategorias(request, response);
                     break;
                 case 2:
-                    crearRegistros(request, response);
+                    recuperarPrioridad(request, response);
                     break;
                 case 3:
+                    crearRegistros(request, response);
+                    break;
+                case 4:
                     //pintarRegistros(request, response);
-                    break;                   
-                case 4:                    
+                    break;
+                case 5:
                     //editarRegistros(request, response);
                     break;
             }
         }
     }
-    
+
     private void recuperarCategorias(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try (PrintWriter out = response.getWriter()) {
-            System.out.println("Entro aqui");
             CategoriaFachada catFachada = new CategoriaFachada();
             List<Categoria> listCategoria = catFachada.getListObject();
-            System.out.println(listCategoria);
             JSONArray array = new JSONArray();
-            for ( Categoria cat : listCategoria ){
-            JSONObject obj = new JSONObject();
-            obj.put("codigo", cat.getCodigo());
-            obj.put("nombre", cat.getNombre());
-            obj.put("codigopadre", cat.getCodigoPadre());
-            obj.put("activo", cat.getActivo());
-            array.add(obj);
+            for (Categoria cat : listCategoria) {
+                JSONObject obj = new JSONObject();
+                obj.put("codigo", cat.getCodigo());
+                obj.put("nombre", cat.getNombre());
+                obj.put("codigopadre", cat.getCodigoPadre());
+                obj.put("activo", cat.getActivo());
+                array.add(obj);
             }
             out.print(array);
         }
     }
-    
+
+    private void recuperarPrioridad(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            PrioridadFachada prioFachada = new PrioridadFachada();
+            List<Prioridad> listPrioridad = prioFachada.getListObject();
+            JSONArray array = new JSONArray();
+            for (Prioridad prio : listPrioridad) {
+                JSONObject obj = new JSONObject();
+                obj.put("codigo", prio.getCodigo());
+                obj.put("nombre", prio.getNombre());
+                obj.put("valor", prio.getValor());
+                obj.put("activo", prio.getActivo());
+                array.add(obj);
+            }
+            out.print(array);
+        }
+    }
+
     private void crearRegistros(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try (PrintWriter out = response.getWriter()) {
             Articulo art = new Articulo();
             art.setTitulo(request.getParameter("tituloClasificado"));
             art.setDescripcion(request.getParameter("cuerpoClasificado"));
             art.setCategoria(new Categoria(Integer.parseInt(request.getParameter("categoria"))));
-            
-            art.setDescripcion(request.getParameter("precioClasificado"));
+            art.setPrecio(Integer.parseInt(request.getParameter("precioClasificado")));
             Usuario usr = (Usuario) request.getSession().getAttribute("user");
             art.setUsuario(usr);
-            //art.setUsuarioCodigo(usr.getCodigo());
-            int cat;
-            cat = Integer.parseInt(request.getParameter("categoria"));
-            art.setFechaPublicacion(null);
-            art.setPrecio(0);
-            art.setFechaFinPublicacion(null);
-            art.setPrioridad(null);
-            ArticuloEstado artEstado = new ArticuloEstado();
-            artEstado.setCodigo(1);
-            art.setEstado(artEstado);
+            art.setPrioridad(new Prioridad(Integer.parseInt(request.getParameter("prioridad"))));
+            art.setPrecio(Integer.parseInt(request.getParameter("precioClasificado")));
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            Date parsed = format.parse(request.getParameter("finPublicacion"));
+            java.sql.Date fechaFin = new java.sql.Date(parsed.getTime());
+            art.setFechaFinPublicacion(fechaFin);
+            art.setEstado(new ArticuloEstado(1));
+            art.setTipoArticulo(new TipoArticulo(1));
+            art.setCategoria(new Categoria(Integer.parseInt(request.getParameter("categoria"))));
             ArticuloFachada artFach = new ArticuloFachada();
-            TipoArticulo tpArt = new TipoArticulo();
-            tpArt.setCodigo(1);
-            art.setTipoArticulo(tpArt);
-            Categoria categ = new Categoria();
-            categ.setCodigo(cat);
-            art.setCategoria(categ);
-            /*if(codArt==null){
-                artFach.insertObject(art);
-            }else{
-                art.setCodigo(Integer.parseInt(codArt));
-                artFach.updateObject(art);
-            }*/
+            artFach.insertObject(art);
+            System.out.println(art);
+            
+        } catch (ParseException ex) {
+            Logger.getLogger(ClasificadoControlador.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
