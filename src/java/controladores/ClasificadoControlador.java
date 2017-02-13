b8957a03526c602adbc4038b18520226d928f4a7
jota09 <jesus.ramos@hmt-colombia.com>
2017-02-13 14:55:46 -0500

    [+] Crear la funcionalidad de inicio.

--- src/java/controladores/ClasificadoControlador.java
+++ src/java/controladores/ClasificadoControlador.java
@@ -235,7 +235,6 @@ public class ClasificadoControlador extends HttpServlet {
     
     private void recuperarClasificado(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try (PrintWriter out = response.getWriter()) {
-            System.out.println("Entro a la opcion 8");
             Articulo art = new Articulo(Integer.parseInt(request.getParameter("id")));
             ArticuloFachada artFachada = new ArticuloFachada();
             art = (Articulo) artFachada.getObject(art);
