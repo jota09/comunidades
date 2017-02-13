dd0502ad253f215fa93a2bbed3747b5382be1845
manuel.alcala <manuel.alcala@HMT-105.hmt.local>
2017-02-13 15:55:20 -0500

    [+]Actualizando...

--- src/java/persistencia/daos/EstructuraDAO.java
+++ src/java/persistencia/daos/EstructuraDAO.java
@@ -20,28 +20,36 @@ import persistencia.entidades.Estructura;
  *
  * @author Jesus.Ramos
  */
-public class EstructuraDAO implements GestionDAO{
+public class EstructuraDAO implements GestionDAO {
 // Este metodo trae uno especifico...
+
     @Override
     public Object getObject(Object object) {
         Estructura estruc = (Estructura) object;
+        Connection con = null;
         try {
-            Connection con=null;
-            con=ConexionBD.obtenerConexion();
-            String query = "SELECT codigo,valor,descripcion FROM estructura WHERE referencia=?";
-            PreparedStatement pS=con.prepareStatement(query);
+            con = ConexionBD.obtenerConexion();
+            String query = "SELECT codigo,referencia,valor,descripcion FROM estructura WHERE referencia=? or codigo=?";
+            PreparedStatement pS = con.prepareStatement(query);
             pS.setString(1, estruc.getReferencia());
-            ResultSet rS=pS.executeQuery();
-            if(rS.next())
-            {
+            pS.setInt(2, estruc.getCodigo());
+            ResultSet rS = pS.executeQuery();
+            if (rS.next()) {
                 estruc.setCodigo(rS.getInt(1));
-                estruc.setValor(rS.getString(2));
-                estruc.setDescripcion(rS.getString(3));
+                estruc.setReferencia(rS.getString(2));
+                estruc.setValor(rS.getString(3));
+                estruc.setDescripcion(rS.getString(4));
             }
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(ArticuloDAO.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
             Logger.getLogger(ArticuloDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(EstructuraDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         return estruc;
     }
@@ -50,29 +58,27 @@ public class EstructuraDAO implements GestionDAO{
     public List getListObject(Object object) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     @Override
     public List getListObject() {
-        ArrayList<Estructura> listEstructura=new ArrayList<Estructura>();                
-        Connection con=null;
-        try
-        {
-            con=ConexionBD.obtenerConexion();
-            String query="SELECT * FROM prioridad";
-            PreparedStatement pS=con.prepareStatement(query);
-            ResultSet rS=pS.executeQuery();            
-            while(rS.next()){
-                Estructura prio=new Estructura(rS.getInt("codigo"), rS.getString("referencia"), rS.getString("valor"), rS.getString("descripcion"));
-                listEstructura.add(prio);
+        ArrayList<Estructura> listEstructura = new ArrayList<Estructura>();
+        Connection con = null;
+        try {
+            con = ConexionBD.obtenerConexion();
+            String query = "SELECT codigo,referencia,valor,descripcion FROM estructura";
+            PreparedStatement pS = con.prepareStatement(query);
+            ResultSet rS = pS.executeQuery();
+            while (rS.next()) {
+                Estructura est = new Estructura(rS.getInt(1), rS.getString(2), rS.getString(3), rS.getString(4));
+                listEstructura.add(est);
             }
             rS.close();
             pS.close();
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-        finally{
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(EstructuraDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(EstructuraDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
             try {
                 con.close();
             } catch (SQLException ex) {
@@ -80,8 +86,8 @@ public class EstructuraDAO implements GestionDAO{
             }
         }
         return listEstructura;
-    }   
-    
+    }
+
     @Override
     public int updateObject(Object object) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -89,16 +95,81 @@ public class EstructuraDAO implements GestionDAO{
 
     @Override
     public int insertObject(Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Estructura estructura = (Estructura) object;
+        Connection con = null;
+        int tamano = 0;
+        try {
+            con = ConexionBD.obtenerConexion();
+            String sql = "Insert into estructura(referencia,valor,descripcion) values(?,?,?)";
+            PreparedStatement pS = con.prepareStatement(sql);
+            pS.setString(1, estructura.getReferencia());
+            pS.setString(2, estructura.getValor());
+            pS.setString(3, estructura.getDescripcion());
+            tamano = pS.executeUpdate();
+            pS.close();
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Error:" + ex.getMessage());
+        } catch (SQLException ex) {
+            System.out.println("Error:" + ex.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                System.out.println("Error:" + ex.getMessage());
+            }
+        }
+        return tamano;
     }
-        @Override
+
+    @Override
     public int getCount(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con = null;
+        int tamano = 0;
+        try {
+            con = ConexionBD.obtenerConexion();
+            String sql = "Select count(codigo) from estructura";
+            PreparedStatement pS = con.prepareStatement(sql);
+            ResultSet rS = pS.executeQuery();
+            if (rS.next()) {
+                tamano = rS.getInt(1);
+            }
+            rS.close();
+            pS.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AtributoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(AtributoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(AtributoDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return tamano;
     }
 
     @Override
     public void deleteObject(Object object) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Estructura estructura = (Estructura) object;
+        Connection con = null;
+        try {
+            con = ConexionBD.obtenerConexion();
+            String sql = "Delete from estructura where codigo=?";
+            PreparedStatement pS = con.prepareStatement(sql);
+            pS.setInt(1, estructura.getCodigo());
+            pS.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(EstructuraDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(EstructuraDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(EstructuraDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     @Override
@@ -110,5 +181,5 @@ public class EstructuraDAO implements GestionDAO{
     public List getListByPagination(Object object) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
