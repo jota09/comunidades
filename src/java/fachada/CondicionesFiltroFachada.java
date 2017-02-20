0d390d1cc21b294ca224ce76459dc0a7dd7a9ed8
manuel.alcala <manuel.alcala@HMT-105.hmt.local>
2017-02-20 15:55:29 -0500

    Merge origin/master
    Conflicts:
    	src/java/persistencia/daos/MultimediaDAO.java

---
+++ src/java/fachada/CondicionesFiltroFachada.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fachada;
+
+import java.util.List;
+import persistencia.daos.CondicionesFiltroDAO;
+import persistencia.daos.GestionDAO;
+
+/**
+ *
+ * @author ALEJO
+ */
+public class CondicionesFiltroFachada implements GestionFachada {
+
+    @Override
+    public int getCount(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getObject(Object object) {
+        GestionDAO condicionDAO=new CondicionesFiltroDAO();
+        return condicionDAO.getObject(object);
+    }
+
+    @Override
+    public int insertObject(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List getListObject(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List getListObject() {
+        GestionDAO condicionesDAO = new CondicionesFiltroDAO();
+        return condicionesDAO.getListObject();
+    }
+
+    @Override
+    public int updateObject(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteObject(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List getListByCondition(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List getListByPagination(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
