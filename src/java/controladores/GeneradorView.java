56f2cf612b9fef606986a9e8d11a8fddbba0dace
manuel.alcala <manuel.alcala@HMT-105.hmt.local>
2017-02-27 10:38:12 -0500

    Merge origin/master
    Conflicts:
    	src/java/controladores/ClasificadoControlador.java

--- src/java/controladores/GeneradorView.java
+++ src/java/controladores/GeneradorView.java
@@ -38,7 +38,6 @@ public class GeneradorView extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session = request.getSession();
         String view = (String) session.getAttribute("view");//getAttribute("view");
-        System.out.println("Vista:" + view);
         if (view == null) {
             view = request.getParameter("view");
         }
