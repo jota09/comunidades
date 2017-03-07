b27a198278c19d19ad72e4bd88d355d2c158fe9b
manuel.alcala <manuel.alcala@HMT-105.hmt.local>
2017-03-07 10:10:17 -0500

    Merge origin/master
    Conflicts:
    	src/java/controladores/SubeArchivoControlador.java

--- src/java/fachada/UsuarioFachada.java
+++ src/java/fachada/UsuarioFachada.java
@@ -14,53 +14,54 @@ import persistencia.daos.UsuarioDAO;
  * @author ferney.medina
  */
 public class UsuarioFachada implements GestionFachada {
-
+    
     public Object getObject(Object obj) {
         GestionDAO usr = new UsuarioDAO();
         return usr.getObject(obj);
     }
-
+    
     @Override
     public int insertObject(Object object) {
         GestionDAO userDAO = new UsuarioDAO();
         return userDAO.insertObject(object);
     }
-
+    
     @Override
     public List getListObject(Object object) {
         GestionDAO userDAO = new UsuarioDAO();
         return userDAO.getListObject(object);
     }
-
+    
     @Override
     public int updateObject(Object obj) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public int getCount(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        GestionDAO userDAO = new UsuarioDAO();
+        return userDAO.getCount(obj);
     }
-
+    
     @Override
     public void deleteObject(Object object) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public List getListObject() {
         GestionDAO userDAO = new UsuarioDAO();
         return userDAO.getListObject();
     }
-
+    
     @Override
     public List getListByCondition(Object object) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
     @Override
     public List getListByPagination(Object object) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    
 }
--- test/prueba.java
+++ test/prueba.java
@@ -1,19 +1,7 @@
 
-import fachada.GestionFachada;
-import fachada.RegistroFachada;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import persistencia.daos.GestionDAO;
-import persistencia.daos.PerfilDAO;
-import persistencia.entidades.Comunidad;
-import persistencia.entidades.Perfil;
-import persistencia.entidades.Registro;
-import utilitarias.Cifrar;
-import utilitarias.LecturaConfig;
-import utilitarias.Utilitaria;
+import persistencia.daos.UsuarioDAO;
+import persistencia.entidades.Usuario;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -29,8 +17,11 @@ public class prueba {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        System.out.println("VALOR:"+Cifrar.getEncryp("8509243222512345", ""));
+    public static void main(String[] args) {
+        GestionDAO usuarioDAO = new UsuarioDAO();
+        Usuario user = new Usuario();
+        user.setCorreo("alejoab12@hotmailcom");
+        System.out.println("EMAIL:" + usuarioDAO.getCount(user));
     }
-
+    
 }
