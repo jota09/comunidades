1f93c4ecaff52461497817db68673c675fe4f703
jota09 <jesus.ramos@hmt-colombia.com>
2017-03-30 12:24:10 -0500

    [+] Creacion de DTO,DAO y facade

--- test/prueba.java
+++ test/prueba.java
@@ -9,6 +9,7 @@ import net.sf.jasperreports.engine.JRException;
 import org.krysalis.barcode4j.impl.code128.EAN128Bean;
 import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
 import org.krysalis.barcode4j.tools.UnitConv;
+import utilitarias.ServicioDeEnvioMail;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -30,37 +31,4 @@ public class prueba {
     }
 
 }
-class metodo {
-    static String metodoRaro(){
-        int[] lista = {8,9,3};
-        int n = lista.length;
-        String[] simbolo = new String[n];
-        int i,j,aux,izq,der,m;
-        
-        for(i=1;i<n;i++){
-            aux = lista[i]; izq = 0; der = i-1;
-            while(izq <= der){
-                m = ((izq+der)/2);
-                if(aux < lista[m]){
-                    der = m-1;
-                    simbolo[i-1] = "-";
-                }else{
-                    izq = m+1;
-                    simbolo[i-1]="+";
-                }
-            }
-            j=i-1;
-            while(j>= izq){
-                lista[j+1] = lista[j];
-                j = j-1;
-            }
-            lista[izq] = aux;
-        }
-        simbolo[i-1] = "$";
-        String salida = "";
-        for(i=0;i<n;i++){
-            salida += lista[i]+ simbolo[i];
-        }
-        return salida;
-    }
-}
+
